346d16efab253849013461783fc7a3e03aa091c0
Kaio Pedro <kaiopedro@alunos.utfpr.edu.br>
2024-05-10 21:25:46 -0300

    [Issue #2] Adicionado models de retorno e account para serilialização, adicionado metodos de ações do accountmanager, tratamento de erros, criado processamentos em services e outros.

--- src/controllers/events.rs
+++ src/controllers/events.rs
@@ -1,11 +1,29 @@
-use actix_web::{ post, web, HttpResponse,Result};
+use std::io::ErrorKind;
+
+use actix_web::{post, web, HttpResponse, Result};
 use serde_json::json;
 
-use crate::models::{AccountManager, DepositRequest};
+use crate::{
+    models::{AccountManager, TransactionRequest},
+    services::process_request,
+};
 
 #[post("/event")]
-async fn transaction_request(req_body: web::Json<DepositRequest>, acc_manager: web::Data<AccountManager>) -> Result<HttpResponse>  {
-    println!("Received deposit request: {:?}", req_body);
-    let response_body = json!({"message": "Deposit request received"});
-    Ok(HttpResponse::Ok().json(response_body))
+async fn transaction_request(
+    req_body: web::Json<TransactionRequest>,
+    acc_manager: web::Data<AccountManager>,
+) -> Result<HttpResponse> {
+    match process_request(req_body.into_inner(), acc_manager) {
+        Ok(transaction_response) => {
+            let response_body = json!(transaction_response);
+            Ok(HttpResponse::Created().json(response_body))
+        }
+        Err(e) => {
+            if let ErrorKind::NotFound = e {
+                Ok(HttpResponse::NotFound().body("0"))
+            } else {
+                Ok(HttpResponse::InternalServerError().finish())
+            }
+        },
+    }
 }
